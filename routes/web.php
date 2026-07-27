53b27f5a5b309ab03c081c62382e119574008202
system-intelligence <system.intelligence2025@gmail.com>
2026-07-27 13:34:09 +0800

    train animation

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
-Route::view('/blog', 'blog')->name('blog');
-Route::view('/blog/dummy-post', 'blog-post')->name('blog.post');
-Route::view('/experience', 'experience')->name('experience');
-Route::view('/skills', 'skills')->name('skills');
-Route::view('/projects', 'projects')->name('projects');
+Route::view('/blog', 'pages.blog')->name('blog');
+Route::view('/blog/dummy-post', 'pages.blog-post')->name('blog.post');
+Route::view('/experience', 'pages.experience')->name('experience');
+Route::view('/skills', 'pages.skills')->name('skills');
+Route::view('/projects', 'pages.projects')->name('projects');
