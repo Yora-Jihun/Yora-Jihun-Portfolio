b119c74949f471743372aaf643d1d28629a41fb9
system-intelligence <system.intelligence2025@gmail.com>
2026-08-06 10:10:37 +0800

    concrete bug fixed from the body of blog - md is now working sheshhh

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -153,7 +153,7 @@
                         observer.unobserve(entry.target);
                     }
                 });
-            }, { threshold: 0.12, rootMargin: '0px 0px -40px 0px' });
+            }, { threshold: 0, rootMargin: '0px 0px -40px 0px' });
 
             document.querySelectorAll('.reveal').forEach(function(el) {
                 observer.observe(el);
